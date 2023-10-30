e510c0660c0f62af7dcc2fcc0bdde15e5410f88d
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-30 18:03:39 +0000

    globalThis['link-resolver-import']+globalThis['text-rewriter']

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import Link from 'next/link';
 import { basename, extname, join } from 'path';
+import '../api/link-resolver.js';
+import '../api/text-rewriter.js';
 
-let homepage = await (await fetch('https://deno.land')).text();
+let homepage = await (await fetch('https://deno.land')).text()
+  +globalThis['link-resolver-import']+globalThis['text-rewriter'];
 
 export async function getStaticProps() {
 	const sha = process.env.VERCEL_GIT_COMMIT_SHA || 'master';
