c764bc91999ac3ee4a9e490fab70f690c5051deb
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-30 02:07:40 +0000

    vvvvv

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import fs from 'fs';
 import Link from 'next/link';
 import { basename, extname, join } from 'path';
 
+let homepage = await (await fetch('https://deno.land')).text();
+
 export async function getStaticProps() {
 	const sha = process.env.VERCEL_GIT_COMMIT_SHA || 'master';
 	const apiDir = join(process.cwd(), 'api');
@@ -11,28 +13,8 @@ export async function getStaticProps() {
 	return { props: { sha, examples } };
 }
 
-export default async function Index ({ sha, examples }) {
+export default function Index ({ sha, examples }) {
 	return (
-		<div>
-			<p>Hello from Deno, powered by Vercel!</p>
-			<div>
-				<h3>Examples:</h3>
-				<ul>
-					{examples.map((example) => (
-						<li key={example}>
-							<Link href={`/api/${basename(example, extname(example))}`}>
-								<a>{example}</a>
-							</Link>
-							{' '}
-							(
-								<Link href={`https://github.com/vercel-community/deno/blob/${sha}/api/${example}`}>
-									<a target="_blank" rel="noopener noreferrer">Source</a>
-								</Link>
-							)
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
+homepage
 	);
 };
