05d13aea079d8ae19f9a8c37fec88bca1747ed8b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-01 13:36:10 +0000

    gkh

--- api/dino.css.js
+++ api/dino.css.js
@@ -26,7 +26,7 @@ visibility:hidden !important;
 }
 </style><script>
 if(document.querySelector('.text-green-500')){
-  document.currentScript.outerHTML="<style>html{filter:hue-rotate(135deg) !important;} img:not([src*=".gif"]),svg,h1,h2,h3,a,:not(pre,code) span[class*="text"]{filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(90deg) !important;}</style>";
+  document.currentScript.outerHTML='<style>html{filter:hue-rotate(135deg) !important;} img:not([src*=".gif"]),svg,h1,h2,h3,a,:not(pre,code) span[class*="text"]{filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(90deg) !important;}</style>';
 }
 </script>`;
 
