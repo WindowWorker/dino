59609adea9c497ca539b65f70ad8e67afe20b917
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-01 15:01:53 +0000

    zxcv

--- api/dino.css.js
+++ api/dino.css.js
@@ -13,7 +13,7 @@ filter:hue-rotate(135deg);
 }
 
 img:not([src*=".gif"]),svg,h1,h2,h3,a:not([href*="docs.deno.com"]),:not(pre,code) span[class*="text"]{
-filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(270deg);
+filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(180deg);
 }
 
 html:has(.text-green-500) img:not([src*=".gif"]),svg,h1,h2,h3,a:not([href*="docs.deno.com"]),:not(pre,code) span[class*="text"]{filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(90deg);}
