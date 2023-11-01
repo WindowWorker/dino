4d1f4748f3d7c53123c8f28a242497d2fc89d42b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-01 15:50:19 +0000

    pkgs[x].setAttribute('fallback',pkgs[x][attr].replace(window.location.host,globalThis.hostTargetList[i]));

--- api/link-resolver.js
+++ api/link-resolver.js
@@ -63,7 +63,7 @@ async function transformLinks(attr){
       let char='?';
       if(pkgs[x][attr].includes('?')){char='&';}
          if(pkgs[x].tagName=='IMG'){
-           pkgs[x].setAttribute('fallback',pkgs[x][attr]);
+           pkgs[x].setAttribute('fallback',pkgs[x][attr].replace(window.location.host,globalThis.hostTargetList[i]));
             pkgs[x].onerror=function(){
               pkgs[x].setAttribute(attr,pkgs[x].getAttribute('fallback'));
             }
@@ -105,7 +105,7 @@ async function transformLinks(attr){
       let char='?';
       if(pkgs[x][attr].includes('?')){char='&';}
        if(pkgs[x].tagName=='IMG'){
-         pkgs[x].setAttribute('fallback',pkgs[x][attr]);
+         pkgs[x].setAttribute('fallback',pkgs[x][attr].replace(window.location.host,globalThis.hostTargetList[i]));
           pkgs[x].onerror=function(){
             pkgs[x].setAttribute(attr,pkgs[x].getAttribute('fallback'));
           }
