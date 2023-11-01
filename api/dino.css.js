b9ac7c14bd01586fd4fc3c48837d63c9c8af636a
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-01 12:49:23 +0000

    :not([src*=".gif"])

--- api/dino.css.js
+++ api/dino.css.js
@@ -12,11 +12,11 @@ filter:hue-rotate(135deg);
 
 }
 
-img,svg,h1,h2,h3,a,[class*="text"]{
+img:not([src*=".gif"]),svg,h1,h2,h3,a,[class*="text"]{
 filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(270deg);
 }
 
-html:has(.text-green-500) img,svg,h1,h2,h3,a,[class*="text"]{filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(90deg);}
+html:has(.text-green-500) img:not([src*=".gif"]),svg,h1,h2,h3,a,[class*="text"]{filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(90deg);}
 
 [href*="/pricing"]{
 display:none !important;
@@ -26,7 +26,7 @@ visibility:hidden !important;
 }
 </style><script>
 if(document.querySelector('.text-green-500')){
-  document.currentScript.outerHTML="<style>html{filter:hue-rotate(135deg) !important;} img,svg,h1,h2,h3,a,[class*="text"]{filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(90deg) !important;}</style>";
+  document.currentScript.outerHTML="<style>html{filter:hue-rotate(135deg) !important;} img:not([src*=".gif"]),svg,h1,h2,h3,a,[class*="text"]{filter: grayscale(1) sepia(1) saturate(1.5) hue-rotate(90deg) !important;}</style>";
 }
 </scr`+`ipt>`;
 
