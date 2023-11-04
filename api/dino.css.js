fb11a018b513906e52415462d884141b080d702d
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-04 14:13:51 +0000

    asdf

--- api/dino.css.js
+++ api/dino.css.js
@@ -12,6 +12,14 @@ filter:hue-rotate(45deg);
 
 }
 
+a[aria-label="Landing Page"][href="https://deno.typescripts.org/"]  {
+  background-image: url("https://upload.wikimedia.org/wikipedia/en/f/f7/Dino_from_%22The_Flintstones%22.gif");
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+
 
 html img:not([src*=".gif"]),svg,h1,h2,h3,a,:not(pre,code) span[class*="text"]{filter: sepia(1) hue-rotate(90deg);}
 
