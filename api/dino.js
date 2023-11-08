28feaa9b53163e306e5f3c7642c5e8126fb7d29b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-08 13:25:01 +0000

    asdf

--- api/dino.js
+++ api/dino.js
@@ -2,7 +2,7 @@
 function Dinoify(){
 
 setInterval(function(){
-let dinos = document.querySelectorAll('img[src*="deno-looking-up.svg"],img[src*="logo.svg"]');
+let dinos = document.querySelectorAll('img[src*="deno-looking-up.svg"],img[src*="logo"][src*=".svg"]');
 let dinos_length = dinos.length;
 
   for(let i=0;i<dinos_length;i++){try{
@@ -10,6 +10,9 @@ let dinos_length = dinos.length;
     dinos[i].src="https://upload.wikimedia.org/wikipedia/en/f/f7/Dino_from_%22The_Flintstones%22.gif";
     
   }catch(e){continue;}}
+
+  let cf = document.querySelector('img[src*="cloudflare-light.svg"]');
+  if(cf){cf.src='https://avatars.githubusercontent.com/u/314135';}
 
   },100);
   
