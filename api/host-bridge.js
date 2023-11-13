9474d33b3724176f82d28cf6e850e4d7a2bd2787
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-13 11:48:30 +0000

    asdf

--- api/host-bridge.js
+++ api/host-bridge.js
@@ -15,28 +15,32 @@ function HostBridge(){
 setInterval(function(){
 
 
-
+  
 },100);
 
 function swapHost(){
 
-  if(window?.location?.href.includes?.('/manual')){
+  if(window?.location?.href.includes?.('/std')
+    ||window?.location?.href.includes?.('/api')
+    ||window?.location?.href.includes?.('@')
+    ||window?.location?.href.includes?.('/manual')
+    ||window?.location?.href.includes?.('hostname=docs.deno.com')){
 
     if(!window.location.href.startsWith('https://docs-')){
       window.location.href=window.location.href.replaceAll(window.location.host,'docs-'+window.location.host);
     }
-
+    
    }else{
 
     if(window.location.href.startsWith('https://docs-')){
       window.location.href=window.location.href.replaceAll('docs-'+window.location.host,window.location.host);
     }
-
+    
    }
-
+  
 }
 
-
+  
 }
 
 
