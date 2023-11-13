cba32940fa6216c34675ce8bf1e801d1efab77e7
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-13 16:10:24 +0000

    asdf

--- api/host-bridge.js
+++ api/host-bridge.js
@@ -15,31 +15,40 @@ function HostBridge(){
 setInterval(function(){
 
 
-  
+
 },100);
 
 function swapHost(){
 
-  if(window?.location?.href.includes?.('/std')
-    ||window?.location?.href.includes?.('/api')
+  if(window?.location?.href.includes?.('/api')
     ||window?.location?.href.includes?.('@')
     ||window?.location?.href.includes?.('/manual')){
 
     if(!(window.location.href.startsWith('https://docs-'))&&!(window.location.href.startsWith('https://std-'))){
       window.location.href=window.location.href.replaceAll(window.location.host,'docs-'+window.location.host).split('?')[0];
     }
-    
+
    }else{
 
     if(window.location.href.startsWith('https://docs-')){
       window.location.href=window.location.href.replaceAll('docs-','').split('?')[0];
     }
-    
+
    }
-  
+
+   if(window?.location?.href.includes?.('/std')){
+     if(!(window.location.href.startsWith('https://std-')||!(window.location.href.startsWith('https://docs-std-')))){
+
+       window.location.href=window.location.href.replaceAll('docs-','').split('?')[0].replace('https://','https://std-');
+
+
+     }
+   }
+
+
 }
 
-  
+
 }
 
 
